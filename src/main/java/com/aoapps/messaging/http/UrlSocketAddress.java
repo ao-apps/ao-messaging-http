455e3b31db00ccfe27d33bb5ed629b60d412656b
AO Industries, Inc <support@aoindustries.com>
2024-10-12 01:15:10 -0500

    checkstyle-10.18.2 compatibility

--- src/main/java/com/aoapps/messaging/http/UrlSocketAddress.java
+++ src/main/java/com/aoapps/messaging/http/UrlSocketAddress.java
@@ -1,6 +1,6 @@
 /*
  * ao-messaging-http - Asynchronous bidirectional messaging over HTTP.
- * Copyright (C) 2014, 2015, 2016, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2014, 2015, 2016, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -35,6 +35,9 @@ public class UrlSocketAddress extends SocketAddress {
 
   private final URL url;
 
+  /**
+   * Creates a new {@link UrlSocketAddress}.
+   */
   public UrlSocketAddress(URL url) {
     this.url = url;
   }
